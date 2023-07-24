8fee59aecaed3e4cc785b946064ff6f72b48fd70
Flopiras <Flopiras@yahoo.it>
2023-07-24 16:30:05 +0200

    add constructor and extendes

--- Models/Item.php
+++ Models/Item.php
@@ -1,4 +1,15 @@
 <?php
-class Item
+
+require_once __DIR__ . '/Element.php';
+
+class Item extends Element
 {
+    private $category;
+    private $price;
+
+    // constructor
+    public function __construct($title, $image, $type, $category, $price)
+    {
+        parent::__construct($title, $image, $type);
+    }
 }
