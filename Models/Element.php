b19429bb5ec4300182be009db400f1d15249b8c9
Flopiras <Flopiras@yahoo.it>
2023-07-24 16:22:24 +0200

    change name Class

--- Models/Element.php
+++ Models/Element.php
@@ -1,6 +1,6 @@
 <?php
 
-class Item
+class Element
 {
     private $title;
     private $image;
--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 include __DIR__ . '/models/Item.php';
 
 
-$item_1 = new Item('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 'food');
+$item_1 = new Element('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 'food');
 
 var_dump($item_1);
