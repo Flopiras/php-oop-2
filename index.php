955b28328e5dd32daaf566006712307dd47de29b
Flopiras <Flopiras@yahoo.it>
2023-07-24 16:34:02 +0200

    test new Item

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-include __DIR__ . '/models/Item.php';
+include_once __DIR__ . '/models/Item.php';
 
 
-$item_1 = new Element('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 'food');
+$item_1 = new item('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 'food', 'dog', 25);
 
 var_dump($item_1);
