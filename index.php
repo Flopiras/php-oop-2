2129813c31bb3432a0a2da9321dfd5b1d457c55a
Flopiras <Flopiras@yahoo.it>
2023-07-24 16:38:33 +0200

    add data folder

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-include_once __DIR__ . '/models/Item.php';
+include_once __DIR__ . '/data/items.php';
 
-
-$item_1 = new item('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 'food', 'dog', 25);
 
 var_dump($item_1);
