fbf01566940739ff6c1fa55272b469b4fe08a4e8
Flopiras <Flopiras@yahoo.it>
2023-07-24 17:02:11 +0200

    add item cards

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 include_once __DIR__ . '/data/items.php';
 
+
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +17,33 @@ include_once __DIR__ . '/data/items.php';
 </head>
 
 <body>
-
+    <div class="container">
+        <header>
+            <h1 class="text-center text-info my-4">Boolshop</h1>
+        </header>
+        <main>
+            <h3>I nostri prodotti</h3>
+            <!-- grid -->
+            <div class="container-fluid">
+                <div class="row">
+                    <?php foreach ($items as $item) : ?>
+                        <div class="col">
+                            <!-- card -->
+                            <div class="card" style="width: 18rem;">
+                                <img src="<?= $item->getImage() ?>" class="card-img-top" alt="<?= $item->getTitle() ?>">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $item->getTitle() ?></h5>
+                                    <p class="card-text"><strong>Category : </strong><?= $item->getCategory() ?></p>
+                                    <p class="card-text"><strong>Price : </strong><?= $item->getPrice() ?></p>
+                                    <a href="#" class="btn btn-primary">Buy Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach  ?>
+                </div>
+            </div>
+        </main>
+    </div>
 </body>
 
 </html>
