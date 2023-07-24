f47cc613aa24cb345605dbebf0213e54f78e641e
Flopiras <Flopiras@yahoo.it>
2023-07-24 16:04:31 +0200

    add new Item

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+include __DIR__ . '/models/Item.php';
+
+
+$item_1 = new Item('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 'dog');
+
+var_dump($item_1);
