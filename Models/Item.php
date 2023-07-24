fc4837a4460f3f6989723a46755fac45e536dc28
Flopiras <Flopiras@yahoo.it>
2023-07-24 15:56:24 +0200

    add type methods

--- Models/Item.php
+++ Models/Item.php
@@ -33,4 +33,15 @@ class Item
     {
         $this->image = $image;
     }
+
+    // type
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
 }
